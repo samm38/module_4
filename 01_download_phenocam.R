88b28c43d0876743e1e0bc82ece0e0c6497c91fe
samm38 <samm38@vt.edu>
2021-03-18 17:39:37 +0000

    download data

---
+++ 01_download_phenocam.R
@@ -0,0 +1,18 @@
+##' Download Phenocam data
+##' 
+##' @param https://phenocam.sr.unh.edu/data/archive/uiefprairie/ROI/uiefprairie_GR_1000_1day.csv  web address where data is located
+download_phenocam <- function(URL) {
+  ## check that we've been passed a URL
+  if (length(URL) == 1 & is.character(URL) & substr(URL, 1, 4) == "http") {
+    
+    ## read data
+    dat <- read_csv(URL, skip = 22)
+    
+    ## convert date
+    dat$date <- as_date(as.character(dat$date))
+    
+    return(dat)
+  } else {
+    print(paste("download.phenocam: Input URL not provided correctly", URL))
+  }
+}
